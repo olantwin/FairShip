6ea8745d029852002cfab07dadc49ff8e61764f5
Oliver Lantwin <oliver.lantwin@cern.ch>
2017-02-23 10:08:18 +0100

    Remove unneeded modules

--- python/shipDet_conf.py
+++ python/shipDet_conf.py
@@ -279,9 +279,8 @@ def configure(run,ship_geo):
  if ship_geo.muShieldDesign==6: fMagField.IncludeTarget(ship_geo.target.xy, ship_geo.target.z0, ship_geo.target.length)
  run.SetField(fMagField)
 #
- exclusionList = []
- #exclusionList = ["Muon","Ecal","Hcal","Strawtubes","TargetTrackers","NuTauTarget","HighPrecisionTrackers",\
- #                 "Veto","Magnet","MuonShield","TargetStation","MagneticSpectrometer","EmuMagnet"]
+ exclusionList = ["Muon","Ecal","Hcal","TargetTrackers","NuTauTarget","HighPrecisionTrackers",\
+                 "Veto","Magnet","TargetStation","MagneticSpectrometer","EmuMagnet"]
  for x in detectorList:
    if x.GetName() in exclusionList: continue
    run.AddModule(x)
