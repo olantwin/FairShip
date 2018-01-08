910190a7ea694b9106b58ce32f33217663514e27
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-01-08 17:44:43 +0100

    Increase field again

--- passive/ShipMuonShield.cxx
+++ passive/ShipMuonShield.cxx
@@ -36,7 +36,7 @@ ShipMuonShield::ShipMuonShield(TString geofile)
   params.Read("params");
   Double_t LE = 10. * m, floor = 5. * m;
   fDesign = 8;
-  fField = 1.7;
+  fField = 1.8;
   dZ0 = 1 * m;
   dZ1 = params[0];
   dZ2 = params[1];
