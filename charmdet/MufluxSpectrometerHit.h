a57f1e7e13a36577026affc12a9a387297706bbf
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-08 23:01:40 +0200

    Fix stupid mistakes

--- charmdet/MufluxSpectrometerHit.h
+++ charmdet/MufluxSpectrometerHit.h
@@ -6,6 +6,13 @@
 #include "TObject.h"
 #include "TVector3.h"
 
+namespace DriftTubes {
+enum Flag : uint16_t {
+   Valid = 0,
+   UnmatchedHeaderTrailer = 1 << 15,
+};
+}
+
 class MufluxSpectrometerHit : public ShipHit {
 public:
    /** Default constructor **/
@@ -25,8 +32,8 @@ public:
    /** Output to screen **/
    virtual void Print() const;
    Float_t tdc() const { return fdigi; }
-   void setInvalid() { flags &= ~DriftTubeFlag::Valid; }
-   bool isValid() const { return flag & DriftTubeFlag::Valid; }
+   void setInvalid() { flags &= ~DriftTubes::Valid; }
+   bool isValid() const { return flags & DriftTubes::Valid; }
 
 private:
    /** Copy constructor **/
@@ -37,11 +44,5 @@ private:
 
    ClassDef(MufluxSpectrometerHit, 3);
 };
-
-enum DriftTubeFlag : uint16_t {
-   None = 0,
-   Valid = 1,
-   UnmatchedHeaderTrailer = 1 << 15,
-}
 
 #endif
