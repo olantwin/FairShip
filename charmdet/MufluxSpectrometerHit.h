ef50d6a4aabadea759da1ed287f041279684da2f
Oliver Lantwin <oliver.lantwin@cern.ch>
2018-07-08 22:10:05 +0200

    Fix flags for DriftTubes, use frametime for RPC

--- charmdet/MufluxSpectrometerHit.h
+++ charmdet/MufluxSpectrometerHit.h
@@ -1,52 +1,47 @@
 #ifndef MUFLUXSPECTROMETERHIT_H
 #define MUFLUXSPECTROMETERHIT_H 1
 
- 
-#include "ShipHit.h" 
-#include "MufluxSpectrometerPoint.h" 
-#include "TObject.h" 
-#include "TVector3.h" 
- 
-class MufluxSpectrometerHit : public ShipHit 
-{ 
-  public: 
- 
-    /** Default constructor **/ 
-    MufluxSpectrometerHit(); 
+#include "ShipHit.h"
+#include "MufluxSpectrometerPoint.h"
+#include "TObject.h"
+#include "TVector3.h"
 
- 
-    /** Constructor with arguments 
-     *@param detID    Detector ID 
-     *@param digi      digitized/measured TDC  
-     *@param flag      True/False, false if there is another hit with smaller tdc  
-     **/ 
-    MufluxSpectrometerHit(Int_t detID, Float_t tdc); 
-    MufluxSpectrometerHit(MufluxSpectrometerPoint* p, Double_t t0); 
-    void MufluxSpectrometerEndPoints(TVector3 &vbot, TVector3 &vtop);   
-/** Destructor **/ 
-    virtual ~MufluxSpectrometerHit(); 
+class MufluxSpectrometerHit : public ShipHit {
+public:
+   /** Default constructor **/
+   MufluxSpectrometerHit();
 
- 
-    /** Output to screen **/ 
-    virtual void Print() const; 
-    Float_t tdc() const {return fdigi;} 
-    void setInvalid() {flag = false;} 
-    bool isValid() const {return flag;} 
+   /** Constructor with arguments
+    *@param detID    Detector ID
+    *@param digi      digitized/measured TDC
+    *@param flag      True/False, false if there is another hit with smaller tdc
+    **/
+   MufluxSpectrometerHit(Int_t detID, Float_t tdc);
+   MufluxSpectrometerHit(MufluxSpectrometerPoint *p, Double_t t0);
+   void MufluxSpectrometerEndPoints(TVector3 &vbot, TVector3 &vtop);
+   /** Destructor **/
+   virtual ~MufluxSpectrometerHit();
 
- 
-    private: 
-    /** Copy constructor **/ 
-    MufluxSpectrometerHit(const MufluxSpectrometerHit& point); 
-    MufluxSpectrometerHit operator=(const MufluxSpectrometerHit& point); 
+   /** Output to screen **/
+   virtual void Print() const;
+   Float_t tdc() const { return fdigi; }
+   void setInvalid() { flags &= ~DriftTubeFlag::Valid; }
+   bool isValid() const { return flag & DriftTubeFlag::Valid; }
 
- 
-    Float_t flag;   ///< flag 
+private:
+   /** Copy constructor **/
+   MufluxSpectrometerHit(const MufluxSpectrometerHit &point);
+   MufluxSpectrometerHit operator=(const MufluxSpectrometerHit &point);
 
- 
-    ClassDef(MufluxSpectrometerHit,3); 
-     
+   uint16_t flags; ///< flag
 
- 
-}; 
+   ClassDef(MufluxSpectrometerHit, 3);
+};
 
-#endif 
+enum DriftTubeFlag : uint16_t {
+   None = 0,
+   Valid = 1,
+   UnmatchedHeaderTrailer = 1 << 15,
+}
+
+#endif
